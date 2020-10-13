cf3be1d0c769befe8ee1286c20f20b5138c2f08f
admin <admin@DESKTOP-M5U9GQI>
2020-10-13 14:42:30 +0900

    add sum of two numbers

---
+++ src/project/sum.java
@@ -0,0 +1,15 @@
+package project;
+
+import java.util.Scanner;
+
+public class sum {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("두 수를 입력하세요.");
+		int firstNum = scanner.nextInt();
+		int secondNum = scanner.nextInt();
+		System.out.println("두 수의 합은 " + (firstNum + secondNum) + "입니다.");
+	}
+
+}
