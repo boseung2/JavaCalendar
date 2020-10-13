f11f9ff3531f4531ea680f283a9f3a4fec1c4725
admin <admin@DESKTOP-M5U9GQI>
2020-10-13 14:07:52 +0900

    Update calendar main()

--- src/project/Calendar.java
+++ src/project/Calendar.java
@@ -2,6 +2,12 @@ package project;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("Hello, Callendar");
+		System.out.println("    일   월   화   수   목   금   토");
+		System.out.println("-----------------------");
+		for (int i = 1; i < 29; i++) {
+			System.out.printf("%3d", i);
+			if (i == 7 || i == 14 || i == 21 || i == 28)
+				System.out.println();
+		}
 	}
 }
